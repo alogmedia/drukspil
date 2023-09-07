52862682199ffea2a508a6f7b3d90e1c6cb0179b
alogmedia <albertlogn@gmail.com>
2023-09-07 10:02:42 +0200

    Opdateret version med login system.

--- index.js
+++ index.js
@@ -1,17 +1,127 @@
 const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 80;
 
-// Middleware for at parse JSON-anmodninger
 app.use(bodyParser.json());
 
-// Konfigurer express.static til at servere statiske filer fra din "public" mappe (eller den relevante mappe)
-app.use(express.static(__dirname));
+// Configure express-session
+app.use(
+  session({
+    secret: 'c2sfOYy4OV4oK5ZUqqyLxiB1XraVjGs3',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+    },
+  })
+);
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Konfigurér cookie-parser
+app.use(cookieParser());
+
+let usersData = {};
+const usersFilePath = path.join(__dirname, 'users.json');
+
+try {
+  const usersFileContent = fs.readFileSync(usersFilePath, 'utf8');
+  usersData = JSON.parse(usersFileContent);
+} catch (error) {
+  console.error('Error reading users.json:', error);
+}
+
+function saveUsersData() {
+  try {
+    fs.writeFileSync(usersFilePath, JSON.stringify(usersData, null, 2), 'utf8');
+  } catch (error) {
+    console.error('Error writing users.json:', error);
+  }
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+
+  if (usersData[username]) {
+    return res.status(400).json({ error: 'Username already exists' });
+  }
+
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
+      return res.status(500).json({ error: 'Error hashing password' });
+    }
+
+    usersData[username] = { password: hashedPassword };
+    saveUsersData();
+
+    return res.status(201).json({ message: 'User registered successfully' });
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+
+  const userData = usersData[username];
+
+  if (!userData) {
+    return res.status(401).json({ error: 'Invalid username or password' });
+  }
+
+  bcrypt.compare(password, userData.password, (err, result) => {
+    if (err || !result) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    // Gem brugeroplysninger i session
+    req.session.user = username;
+
+    // Opret en cookie for at indikere, at brugeren er logget ind
+    res.cookie('isLoggedIn', 'true');
+
+    // Authentication successful
+    return res.status(200).json({ message: 'Login successful' });
+  });
+});
+
+app.post('/logout', (req, res) => {
+  // Fjern brugerens session eller nulstil den som passende (afhænger af din sessionhåndtering)
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Fejl ved log ud:', err);
+      return res.status(500).json({ error: 'Fejl ved log ud' });
+    }
+    return res.status(200).json({ message: 'Logout successful' });
+  });
+});
+
+app.get('/statements.json', (req, res) => {
+  const filePath = path.join(__dirname, 'public/statements.json');
+  res.sendFile(filePath);
+});
 
 app.post('/add-statement', (req, res) => {
   const newStatement = req.body.statement;
@@ -20,27 +130,22 @@ app.post('/add-statement', (req, res) => {
     return res.status(400).json({ error: 'Ugyldig påstand' });
   }
 
-  // Opret den fulde sti til statements.json-filen
   const filePath = path.join(__dirname, 'public/statements.json');
 
   try {
-    // Læs eksisterende påstande fra filen
     const statementsData = fs.readFileSync(filePath, 'utf8');
     const statements = JSON.parse(statementsData);
 
-    // Tilføj den nye påstand til arrayet
     statements.statements.push(newStatement);
 
-    // Gem de opdaterede påstande til filen
     fs.writeFileSync(filePath, JSON.stringify(statements, null, 2), 'utf8');
 
     return res.status(200).json({ message: 'Påstand tilføjet med succes' });
   } catch (error) {
     console.error('Fejl ved tilføjelse af påstand:', error);
-    return res.status(500).json({ error: 'Intern serverfejl' }); // Tilføj en passende fejlmeddelelse
+    return res.status(500).json({ error: 'Intern serverfejl' });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Serveren kører på port ${port}`);
--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,8 +1,36 @@
 let statements = []; // Array til at gemme påstande
 
+// Når siden indlæses
+window.addEventListener('load', () => {
+  // Tjek om brugeren er logget ind ved at se efter cookien
+  const isLoggedIn = document.cookie.includes('isLoggedIn=true');
+  
+  if (isLoggedIn) {
+    // Brugeren er logget ind, skjul registrerings- og login-formularerne
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('registerForm').style.display = 'none';
+
+    // Vis brugerens points eller andre relevante oplysninger
+    document.getElementById('pointsDisplay').style.display = 'block';
+
+    // Vis log ud knappen
+    document.getElementById('logoutButton').style.display = 'block';
+  } else {
+    // Brugeren er ikke logget ind, vis registrerings- og login-formularerne
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('registerForm').style.display = 'block';
+
+    // Skjul brugerens points eller andre relevante oplysninger
+    document.getElementById('pointsDisplay').style.display = 'none';
+
+    // Skjul log ud knappen
+    document.getElementById('logoutButton').style.display = 'none';
+  }
+});
+
 function loadStatements() {
   return new Promise((resolve, reject) => {
-    fetch('/public/statements.json')
+    fetch('statements.json')
       .then((response) => response.json())
       .then((data) => {
         statements = data.statements;
@@ -15,112 +43,196 @@ function loadStatements() {
   });
 }
 
-// Kald loadStatements-funktionen og vent på, at det er fuldført
-loadStatements()
-  .then(() => {
-    const statementElement = document.getElementById('statement');
-    const nextButton = document.getElementById('next');
-    const newStatementInput = document.getElementById('newStatement');
-    const addButton = document.getElementById('add');
+loadStatements().then(() => {
+  const statementElement = document.getElementById('statement');
+  const newStatementInput = document.getElementById('newStatement');
+  const addButton = document.getElementById('add');
 
-    let currentStatementIndex = 0;
+  let currentStatementIndex = 0;
+
+  // Set the initial statement when the page loads
+  statementElement.textContent = statements[currentStatementIndex];
+    // Resten af din kode...
+
+    // Funktion til at læse cookien med navnet "points"
+    function getPointsFromCookie() {
+      const name = "pointsDisplay=";
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const cookieArray = decodedCookie.split(';');
+      for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+          cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+          return parseInt(cookie.substring(name.length), 10);
+        }
+      }
+      return 0; // Returner 0 point, hvis cookien ikke findes
+    }
+
+    // Funktion til at opdatere pointene og gemme dem i en cookie
+    function updatePoints(points) {
+      document.getElementById('pointsDisplay').textContent = `Points: ${points}`;
+      document.cookie = `pointsDisplay=${points}`;
+    }
+
+    // Kald funktionen for at indlæse eksisterende point og opdatere visningen
+    let currentPoints = getPointsFromCookie();
+    updatePoints(currentPoints);
+
+    // Eventlistener for "Næste" knappen
+    const nextButton = document.getElementById('next');
+    let isNextButtonEnabled = true;
 
     nextButton.addEventListener('click', () => {
-      if (statements.length === 0) {
-        statementElement.textContent = 'Ingen påstande tilføjet endnu.';
-      } else {
-        currentStatementIndex = (currentStatementIndex + 1) % statements.length;
+      if (isNextButtonEnabled) {
+        currentStatementIndex++;
+        if (currentStatementIndex >= statements.length) {
+          currentStatementIndex = 0; // Wrap around to the first statement
+        }
         statementElement.textContent = statements[currentStatementIndex];
+
+        // Update the points when "Næste" is clicked
+        currentPoints++;
+        updatePoints(currentPoints);
+        // Deaktiver knappen i et stykke tid (f.eks. 2 sekunder)
+        nextButton.disabled = true;
+        isNextButtonEnabled = false;
+        setTimeout(() => {
+          nextButton.disabled = false;
+          isNextButtonEnabled = true;
+        }, 2000); // 2 sekunder
       }
     });
 
-    addButton.addEventListener('click', () => {
-      const newStatement = newStatementInput.value;
-      if (newStatement.trim() !== '') {
-        statements.push(newStatement);
-        newStatementInput.value = '';
-        currentStatementIndex = statements.length - 1;
-        statementElement.textContent = newStatement;
+    // Her kan du tilføje logikken for at tilføje en ny påstand
+    const newStatement = newStatementInput.value;
+    if (newStatement.trim() !== '') {
+      statements.push(newStatement);
+      newStatementInput.value = '';
+      currentStatementIndex = statements.length - 1;
+      statementElement.textContent = newStatement;
 
-        // Send den nye påstand til serveren via en POST-anmodning
-        fetch('/add-statement', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ statement: newStatement }),
+      // Send den nye påstand til serveren via en POST-anmodning
+      fetch('/add-statement', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ statement: newStatement }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.message === 'Påstand tilføjet med succes') {
+            // Behandle bekræftelse her, hvis nødvendigt
+          } else {
+            // Håndter fejl her, hvis nødvendigt
+          }
         })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.message === 'Påstand tilføjet med succes') {
-              // Behandle bekræftelse her, hvis nødvendigt
-            } else {
-              // Håndter fejl her, hvis nødvendigt
-            }
-          })
-          .catch((error) => {
-            console.error('Fejl ved POST-anmodning:', error);
-          });
-      }
-    });
-
-    // ... (Resten af din kode)
+        .catch((error) => {
+          console.error('Fejl ved POST-anmodning:', error);
+        });
+    }
   })
   .catch((error) => {
-    console.error('Fejl ved indlæsning af JSON-fil:', error);
+    // Handle error here if needed
+    console.error('Error loading statements:', error);
   });
 
-  // Funktion til at læse cookien med navnet "points"
-  function getPointsFromCookie() {
-    const name = "points=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const cookieArray = decodedCookie.split(';');
-    for (let i = 0; i < cookieArray.length; i++) {
-      let cookie = cookieArray[i];
-      while (cookie.charAt(0) === ' ') {
-        cookie = cookie.substring(1);
+// Resten af din kode...
+
+// Find logud-knappen ved hjælp af dens ID
+const logoutButton = document.getElementById('logoutButton');
+
+// Tilføj en eventlistener til logud-knappen
+logoutButton.addEventListener('click', () => {
+  // Send en anmodning til serveren for at logge brugeren ud (dette afhænger af din serverimplementering)
+  fetch('/logout', {
+    method: 'POST', // Du kan bruge POST eller en anden passende metode
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // Eventuelle data, du vil sende til serveren, kan tilføjes her
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message === 'Logout successful') {
+        // Håndter logud-success her (f.eks. omdiriger brugeren til loginsiden)
+        console.log('Log ud vellykket');
+        // Fjern isLoggedIn-cookien
+        document.cookie = 'isLoggedIn=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'; // Udløber datoen i fortiden for at slette cookien
+        // For eksempel kan du omdirigere brugeren til loginsiden:
+        window.location.href = '/'; // Ændr stien til din login-side
+      } else {
+        // Håndter logud-fejl her
+        console.error('Logud-fejl:', data.message);
       }
-      if (cookie.indexOf(name) === 0) {
-        return parseInt(cookie.substring(name.length), 10);
-      }
-    }
-    return 0; // Returner 0 point, hvis cookien ikke findes
-  }
+    })
+    .catch((error) => {
+      console.error('Fejl ved logud:', error);
+    });
+});
 
-  // Funktion til at opdatere pointene og gemme dem i en cookie
-  function updatePoints(points) {
-    document.getElementById('points').textContent = `Point: ${points}`;
-    document.cookie = `points=${points}`;
-  }
+// Event listener for the login form submission
+loginForm.addEventListener('submit', async (event) => {
+  event.preventDefault(); // Prevent the default form submission behavior
 
-  // Kald funktionen for at indlæse eksisterende point og opdatere visningen
-  let currentPoints = getPointsFromCookie();
-  updatePoints(currentPoints);
+  // Extract the username and password from the form
+  const loginUsername = document.getElementById('loginUsername').value;
+  const loginPassword = document.getElementById('loginPassword').value;
 
-  // Eventlistener for "Næste" knappen
-  const nextButton = document.getElementById('next');
-  nextButton.addEventListener('click', () => {
-    currentPoints++;
-    updatePoints(currentPoints);
-  });
+  try {
+    // Send a POST request to the server for login
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: loginUsername, password: loginPassword }),
+    });
 
-  // Eventlistener for "Tilføj påstand" knappen
-  const addButton = document.getElementById('add');
-  addButton.addEventListener('click', () => {
-    if (currentPoints >= 10) {
-      // Hvis brugeren har nok point, træk 10 point og tilføj påstanden
-      currentPoints -= 10;
-      updatePoints(currentPoints);
-
-      // Her kan du tilføje logikken for at tilføje en ny påstand
+    if (response.ok) {
+      // Authentication successful, update the UI as needed
+      // For example, you can hide the login form and show user-specific content
+      console.log('Login successful');
     } else {
-      // Hvis brugeren ikke har nok point, vis en besked eller udfør en handling
-      alert('Du har ikke nok point til at tilføje en påstand.');
+      // Authentication failed, show an error message or handle it accordingly
+      console.error('Login failed');
     }
-  });
+  } catch (error) {
+    console.error('Error during login:', error);
+  }
+});
 
-// Resten af din eksisterende kode...
+// Event listener for the registration form submission
+registerForm.addEventListener('submit', async (event) => {
+  event.preventDefault(); // Prevent the default form submission behavior
 
+  // Extract the username and password from the form
+  const registerUsername = document.getElementById('registerUsername').value;
+  const registerPassword = document.getElementById('registerPassword').value;
 
-// Kald loadStatements-funktionen, når siden indlæses
-window.addEventListener('load', loadStatements);
+  try {
+    // Send a POST request to the server for registration
+    const response = await fetch('/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: registerUsername, password: registerPassword }),
+    });
+
+    if (response.ok) {
+      // Registration successful, update the UI as needed
+      // For example, you can hide the registration form and show user-specific content
+      console.log('Registration successful');
+      alert("Du har registreret en konto og du kan nu logge ind!")
+    } else {
+      // Registration failed, show an error message or handle it accordingly
+      console.error('Registration failed');
+    }
+  } catch (error) {
+    console.error('Error during registration:', error);
+  }
+});
